05b5488032ce8e594f08236ac6b13607aba6444e
ameliechamp <ac2242@kent.ac.uk>
2024-10-18 13:46:03 +0100

    Update PartTwo.py

--- PartTwo.py
+++ PartTwo.py
@@ -1,1 +1,8 @@
-
+money = 0
+change = 0
+while money <= 75:
+    moneyinput = int(input("Enter money:"))
+    money += moneyinput
+    print(money)
+    change = money - 75
+print("You entered in:",money,". Your change is:", change)
