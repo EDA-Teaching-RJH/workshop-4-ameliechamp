bcab5d447acde6f5bae973baefb88094d6a18af6
ameliechamp <ac2242@kent.ac.uk>
2024-10-18 13:45:41 +0100

    Update PartOne.py

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,8 @@
-
+camel = input("Enter in your variable in camel case: ")
+outputstr=""
+for c in camel:
+    if c.islower():
+        outputstr+=c
+    else:
+        outputstr += "_" + c.lower()
+print(outputstr)
