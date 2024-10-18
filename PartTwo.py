53230329d5f90a75b0e92f710fda8700ac8c3087
ameliechamp <ac2242@kent.ac.uk>
2024-10-18 13:56:46 +0100

    Update PartTwo.py

--- PartTwo.py
+++ PartTwo.py
@@ -1,6 +1,28 @@
 money = 0
 change = 0
-while money <= 75:
+coffee = 75
+hotchocolate = 80
+tea = 70
+shot = 5
+choice = input("Would you like tea, coffee or hot chocolate?: ").lower()
+if choice == "tea":
+    option = tea
+elif choice == "coffee":
+    option = coffee
+    extra = input("Would you like to add a shot to your coffee?: ").lower()
+    if extra == "yes":
+        option = option + shot
+    elif extra == "no":
+        print("Nevermind.")
+    else:
+        print("Nevermnd.")
+elif choice == "hot chocolate":
+    option = hotchocolate
+else:
+    print("Please enter a drink we have.")
+
+print("Please enter in", option, "p")
+while money <= option:
     moneyinput = int(input("Enter money:"))
     money += moneyinput
     print(money)
